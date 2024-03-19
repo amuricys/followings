f16d376b531386174c32455739dbfc345d043393
Deco <decomuricy@gmail.com>
2024-03-18 21:56:54 -0400

    there aint nothin in the first two chapters

---
+++ theorem-proving-in-lean4/TheoremProvingInLean4/DependentTypeTheory.lean
@@ -0,0 +1,136 @@
+/- Define some constants. -/
+
+def m : Nat := 1       -- m is a natural number
+def n : Nat := 0
+def b1 : Bool := true  -- b1 is a Boolean
+def b2 : Bool := false
+
+/- Check their types. -/
+
+#check m            -- output: Nat
+#check n
+#check n + 0        -- Nat
+#check m * (n + 0)  -- Nat
+#check b1           -- Bool
+#check b1 && b2     -- "&&" is the Boolean and
+#check b1 || b2     -- Boolean or
+#check true         -- Boolean "true"
+
+/- Evaluate -/
+
+#eval 5 * 4         -- 20
+#eval m + 2         -- 3
+#eval b1 && b2      -- false
+
+#check Nat → Nat      -- type the arrow as "\to" or "\r"
+#check Nat -> Nat     -- alternative ASCII notation
+
+#check Nat × Nat      -- type the product as "\times"
+#check Prod Nat Nat   -- alternative notation
+
+#check Nat → Nat → Nat
+#check Nat → (Nat → Nat)  --  same type as above
+
+#check Nat × Nat → Nat
+#check (Nat → Nat) → Nat -- a "functional"
+
+#check Nat.succ     -- Nat → Nat
+#check (0, 1)       -- Nat × Nat
+#check Nat.add      -- Nat → Nat → Nat
+
+#check Nat.succ 2   -- Nat
+#check Nat.add 3    -- Nat → Nat
+#check Nat.add 5 2  -- Nat
+#check (5, 9).1     -- Nat
+#check (5, 9).2     -- Nat
+
+#eval Nat.succ 2   -- 3
+#eval Nat.add 5 2  -- 7
+#eval (5, 9).1     -- 5
+#eval (5, 9).2     -- 9
+
+#check Nat               -- Type
+#check Bool              -- Type
+#check Nat → Bool        -- Type
+#check Nat × Bool        -- Type
+#check Nat → Nat         -- ...
+#check Nat × Nat → Nat
+#check Nat → Nat → Nat
+#check Nat → (Nat → Nat)
+#check Nat → Nat → Bool
+#check (Nat → Nat) → Nat
+
+def α : Type := Nat
+def β : Type := Bool
+def F : Type → Type := List
+def G : Type → Type → Type := Prod
+
+#check α        -- Type
+#check F α      -- Type
+#check F Nat    -- Type
+#check G α      -- Type → Type
+#check G α β    -- Type
+#check G α Nat  -- Type
+
+#check (Nat → Type) -- Type 1
+#check (let A := Type; λ a : A ↦ a) -- Type → Type
+
+/-
+sort	| Prop (Sort 0)	Type (Sort 1)	Type 1 (Sort 2)	Type 2 (Sort 3)	        ...
+type	| True	        Bool          Nat -> Type	    Type -> Type 1	        ...
+term	| trivial	      true          fun n => Fin n	fun (_ : Type) => Type	...
+-/
+
+-- variable (α β γ : Type)
+-- variable (g : β → γ) (f : α → β) (h : α → α)
+-- variable (x : α)
+
+-- def compose := g (f x)
+-- def doTwice := h (h x)
+-- def doThrice := h (h (h x))
+
+-- #print compose
+-- #print doTwice
+-- #print doThrice
+
+section useful
+  variable (α β γ : Type)
+  variable (g : β → γ) (f : α → β) (h : α → α)
+  variable (x : α)
+
+  def compose' := g (f x)
+  def doTwice' := h (h x)
+  def doThrice' := h (h (h x))
+end useful
+
+namespace Foo
+  def a : Nat := 5
+  def f (x : Nat) : Nat := x + 7
+
+  def fa : Nat := f a
+  def ffa : Nat := f (f a)
+
+  #check a
+  #check f
+  #check fa
+  #check ffa
+  #check Foo.fa
+end Foo
+
+-- #check a  -- error
+-- #check f  -- error
+#check Foo.a
+#check Foo.f
+#check Foo.fa
+#check Foo.ffa
+
+open Foo
+
+#check a
+#check f
+#check fa
+#check Foo.fa
+
+-- Wtf is this book lol it's so poorly written.
+-- feels very brazilian
+
