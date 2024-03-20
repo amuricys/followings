0de4657e64892b0a856095859ccb8e077cc1e5de
Deco <decomuricy@gmail.com>
2024-03-19 22:21:56 -0400

    Finish chap 3. Start 4. Then tactics and interacting then back to poly

---
+++ theorem-proving-in-lean4/TheoremProvingInLean4/PropositionsAsTypes.lean
@@ -0,0 +1,89 @@
+def Implies (p q : Prop) : Prop := p → q
+#check And     -- Prop → Prop → Prop
+#check Or      -- Prop → Prop → Prop
+#check Not     -- Prop → Prop
+#check Implies -- Prop → Prop → Prop
+
+variable (p q r : Prop)
+#check And p q                      -- Prop
+#check Or (And p q) r               -- Prop
+#check Implies (And p q) (And q p)  -- Prop
+
+variable {p : Prop}
+variable {q : Prop}
+theorem t1 : p → q → p :=
+  fun hp : p =>
+  fun hq : q =>
+  show p from hp
+
+variable (p q : Prop)
+
+theorem and_swap : p ∧ q ↔ q ∧ p :=
+  Iff.intro
+    (fun h : p ∧ q =>
+     show q ∧ p from And.intro (And.right h) (And.left h))
+    (fun h : q ∧ p =>
+     show p ∧ q from And.intro (And.right h) (And.left h))
+
+#check and_swap p q    -- p ∧ q ↔ q ∧ p
+
+variable (h : p ∧ q)
+example : q ∧ p := Iff.mp (and_swap p q) h
+
+variable (p q : Prop)
+
+example (h : p ∧ q) : q ∧ p :=
+  let hp : p := h.left
+  have hq : q := h.right
+  show q ∧ p from And.intro hq hp
+
+open Classical
+
+-- theorem dne {p : Prop} (h : ¬¬p) : p :=
+--   Or.elim (em p)
+--     (λ hp : p ↦ hp)
+--     (λ hnp : ¬p ↦ absurd hnp h)
+
+
+-- exercises from PLFA lol: show EM from DNE
+axiom dne {p : Prop} (h : ¬¬p) : p
+
+/-
+postulate
+  dne : ∀ {p : Type lzero} → ¬ (¬ p) → p
+
+lem : ∀ {p : Type lzero} → (p ⊎ ¬ p)
+lem {p} = dne k
+  where k : ¬ ¬ (p ⊎ ¬ p)
+        k x = x (inr (λ x₁ → x (inl x₁)))
+
+peirce : ∀ {p q : Type lzero} → ((p → q) → p) → p
+peirce {p} {q} = dne k
+  where k : ¬ ¬ (((p → q) → p) → p)
+        k w = w -- w here is assuming that peirce WASN'T true.
+              λ ⦅p→q⦆→p → -- So it takes a function whose assumption is that peirce IS true
+              ⦅p→q⦆→p -- this function takes an implication p → q
+              λ (p1 : p) → -- So that's this lambda. the argument is of type p
+              -- and now we need to give a q, which we need from the ether.
+              -- Needing things from the ether is usually call for `absurd`.
+
+              absurd {A = q {- we need a q -} }
+
+                -- absurd needs a ⊥. The only thing that generates it is w
+                -- recall that w is: "what if peirce wasn't true". So it takes
+                -- "peirce is true" as an argument. We will construct that.
+                (w
+                  -- a value of "peirce is true" is a function that takes p→q→p
+                  -- and returns p.
+                  λ (x : (p → q) → p) →
+                  -- But wait, we assumed p forever ago. So we can just return that.
+                  p1
+                  )
+
+imp-as-disj : ∀ {p q : Type lzero} → (p → q) → ¬ p ⊎ q
+imp-as-disj {p} {q} = dne k
+   where
+       k : ¬ ¬ ((p → q) → ¬ (p ⊎ q))
+       k x = {!   !}
+-/
+#check 1
---
+++ theorem-proving-in-lean4/TheoremProvingInLean4/Quantification.lean
@@ -0,0 +1,78 @@
+example (α : Type) (p q : α → Prop) :
+  (∀ x : α, p x ∧ q x) → ∀ y : α, p y :=
+  λ x ↦ λ y ↦ (x y).left
+
+variable (α : Type) (r : α → α → Prop)
+
+variable (refl_r : ∀ x, r x x)
+variable (symm_r : ∀ {x y}, r x y → r y x)
+variable (trans_r : ∀ {x y z}, r x y → r y z → r x z)
+
+example (a b c d : α) (hab : r a b) (hcb : r c b) (hcd : r c d) : r a d :=
+  trans_r (trans_r hab (symm_r hcb)) hcd
+
+/-
+It is the typing rule for dependent arrow types, and the universal
+quantifier in particular, that distinguishes Prop from other types.
+Suppose we have α : Sort i and β : Sort j, where the expression β
+may depend on a variable x : α. Then (x : α) → β is an element of
+Sort (imax i j), where imax i j is the maximum of i and j if j is
+not 0, and 0 otherwise.
+-/
+
+universe u
+
+#check @Eq.refl.{u}   -- @Eq.refl : ∀ {α : Sort u} (a : α), a = a
+#check @Eq.symm.{u}   -- @Eq.symm : ∀ {α : Sort u} {a b : α}, a = b → b = a
+#check @Eq.trans.{u}  -- @Eq.trans : ∀ {α : Sort u} {a b c : α}, a = b → b = c → a = c
+
+variable (α β : Type)
+example (f : α → β) (a : α) : (fun x => f x) a = f a := rfl
+example (a : α) (b : β) : (a, b).1 = a := rfl
+example : 2 + 3 = 5 := rfl
+
+-- NICE
+example (α : Type) (a b : α) (p : α → Prop)
+        (h1 : a = b) (h2 : p a) : p b :=
+  Eq.subst h1 h2
+
+example (α : Type) (a b : α) (p : α → Prop)
+    (h1 : a = b) (h2 : p a) : p b :=
+  h1 ▸ h2
+
+variable (α : Type)
+variable (a b : α)
+variable (f g : α → Nat)
+variable (h₁ : a = b)
+variable (h₂ : f = g)
+
+-- so goddamn simple
+example : f a = f b := congrArg f h₁
+example : f a = g a := congrFun h₂ a
+example : f a = g b := congr h₂ h₁
+
+variable (a b c : Nat)
+
+-- yeessss
+example : a + 0 = a := Nat.add_zero a
+example : 0 + a = a := Nat.zero_add a
+example : a * 1 = a := Nat.mul_one a
+example : 1 * a = a := Nat.one_mul a
+example : a + b = b + a := Nat.add_comm a b
+example : a + b + c = a + (b + c) := Nat.add_assoc a b c
+example : a * b = b * a := Nat.mul_comm a b
+example : a * b * c = a * (b * c) := Nat.mul_assoc a b c
+example : a * (b + c) = a * b + a * c := Nat.mul_add a b c
+example : a * (b + c) = a * b + a * c := Nat.left_distrib a b c
+example : (a + b) * c = a * c + b * c := Nat.add_mul a b c
+example : (a + b) * c = a * c + b * c := Nat.right_distrib a b c
+
+
+example (x y : Nat) : (x + y) * (x + y) = x * x + y * x + x * y + y * y :=
+  -- Distribution
+  have h1 : (x + y) * (x + y) = (x + y) * x + (x + y) * y :=
+    Nat.mul_add (x + y) x y
+  -- wat?
+  have h2 : (x + y) * (x + y) = x * x + y * x + (x * y + y * y) :=
+    (Nat.add_mul x y x) ▸ (Nat.add_mul x y y) ▸ h1
+  h2.trans (Nat.add_assoc (x * x + y * x) (x * y) (y * y)).symm
